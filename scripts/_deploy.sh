fe07c1362f36af3255a0729cc82937fd9e8e1e9f
0xFar5eer <0xfar5eer@gmail.com>
2022-11-24 00:27:07 +0700

    Update _deploy.sh

--- scripts/_deploy.sh
+++ scripts/_deploy.sh
@@ -14,6 +14,7 @@ STAKE_VOTING_CODE_HASH=$(echo "$STAKE_VOTING_CODE_HASH" | grep hash | tail -1 | 
 echo "Stake voting code hash: $STAKE_VOTING_CODE_HASH"
 
 STAKE_VOTING=$(cargo contract instantiate --url "$NODE_URL" --suri "$AUTHORITY_SEED" --code-hash "$STAKE_VOTING_CODE_HASH" --constructor default --skip-confirm)
+# STAKE_VOTING=$(cargo contract instantiate --url "wss://ws.test.azero.dev" --suri "0xe5be9a5092b81bca64be81d212e7f2f9eba183bb7a90954f7b76361f6edb5c0a" --code-hash "0xd8c7192a5b78d4009f19d8f7c1b88a281592028add3117ad22c4e810fb043a04" --constructor default --skip-confirm)
 echo "$STAKE_VOTING\n\n"
 
 STAKE_VOTING=$(echo "$STAKE_VOTING" | grep -A3 "Event Contracts ➜ Instantiated" | grep contract | tail -1 | cut -d ' ' -f11)
