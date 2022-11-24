a6a67d4b14fcd15e3270718242c5047202b0dd21
0xFar5eer <0xfar5eer@gmail.com>
2022-11-24 18:08:25 +0700

    Update _deploy.sh

--- scripts/_deploy.sh
+++ scripts/_deploy.sh
@@ -21,32 +21,32 @@ echo "$STAKE_VOTING\n\n"
 STAKE_VOTING=$(echo "$STAKE_VOTING" | grep -A3 "Event Contracts ➜ Instantiated" | grep contract | tail -1 | cut -d ' ' -f11)
 echo "Stake voting instance address: $STAKE_VOTING"
 
-cargo contract call --url --url "$NODE_URL" --suri "$AUTHORITY_SEED" --contract $STAKE_VOTING -m "add_one_stake_operator" \
+cargo contract call --url "$NODE_URL" --suri "$AUTHORITY_SEED" --contract $STAKE_VOTING -m "add_one_stake_operator" \
  --args "{ stake_operator_id: 5Eo5ZxVUGbT6D8cfAvAxQFhzt3ZqBEb5oE8KCWR9vjvTPSMy, name: \"AZF/SHANNON\", own_staked: 642_722, other_staked: 3_218_000, commission: 0, total_stakers: 618, vote_points: 0 }" --skip-confirm
 
-cargo contract call --url --url "$NODE_URL" --suri "$AUTHORITY_SEED" --contract $STAKE_VOTING -m "add_one_stake_operator" \
+cargo contract call --url "$NODE_URL" --suri "$AUTHORITY_SEED" --contract $STAKE_VOTING -m "add_one_stake_operator" \
  --args "{ stake_operator_id: 5GW5kbpuYn8Wa2253xLNLn9dZYWJUPJmW7VwmjnziDWdGxiX, name: \"AZF/SIERPINSKI\", own_staked: 73_606, other_staked: 334_565, commission: 0, total_stakers: 509, vote_points: 0 }" --skip-confirm
 
-cargo contract call --url --url "$NODE_URL" --suri "$AUTHORITY_SEED" --contract $STAKE_VOTING -m "add_one_stake_operator" \
+cargo contract call --url "$NODE_URL" --suri "$AUTHORITY_SEED" --contract $STAKE_VOTING -m "add_one_stake_operator" \
  --args "{ stake_operator_id: 5CGTtuqDbBQokPQjpa4mQyNKyvYxKpgtZEskDkJxzho1NhbM, name: \"AZF/CANTOR\", own_staked: 229_798, other_staked: 567_941, commission: 0, total_stakers: 515, vote_points: 0 }" --skip-confirm
 
-cargo contract call --url --url "$NODE_URL" --suri "$AUTHORITY_SEED" --contract $STAKE_VOTING -m "add_one_stake_operator" \
+cargo contract call --url "$NODE_URL" --suri "$AUTHORITY_SEED" --contract $STAKE_VOTING -m "add_one_stake_operator" \
  --args "{ stake_operator_id: 5HYzfrjAMGB6zWW3oTg7dhGdWB8cawyU84fCpGar9QhupweS, name: \"AZF/HILBERT\", own_staked: 140_053, other_staked: 303_097, commission: 0, total_stakers: 515, vote_points: 0 }" --skip-confirm
 
-cargo contract call --url --url "$NODE_URL" --suri "$AUTHORITY_SEED" --contract $STAKE_VOTING -m "add_one_stake_operator" \
+cargo contract call --url "$NODE_URL" --suri "$AUTHORITY_SEED" --contract $STAKE_VOTING -m "add_one_stake_operator" \
  --args "{ stake_operator_id: 5Dkh7kuPm4NMfkmDG1LaVZVWXW3WHYwh7BKEFfNvPiGDrARH, name: \"AZF/FERMAT\", own_staked: 142_356, other_staked: 498_871, commission: 0, total_stakers: 511, vote_points: 0 }" --skip-confirm
 
-cargo contract call --url --url "$NODE_URL" --suri "$AUTHORITY_SEED" --contract $STAKE_VOTING -m "add_one_stake_operator" \
+cargo contract call --url "$NODE_URL" --suri "$AUTHORITY_SEED" --contract $STAKE_VOTING -m "add_one_stake_operator" \
  --args "{ stake_operator_id: 5HNnDD5djTaiUt3A6yf6f1E9oDiM5w5fcNBTLLCoMKf1TEdS, name: \"AZF/RAMANUJAN\", own_staked: 300_787, other_staked: 699_972, commission: 0, total_stakers: 515, vote_points: 0 }" --skip-confirm
 
-cargo contract call --url --url "$NODE_URL" --suri "$AUTHORITY_SEED" --contract $STAKE_VOTING -m "add_one_stake_operator" \
+cargo contract call --url "$NODE_URL" --suri "$AUTHORITY_SEED" --contract $STAKE_VOTING -m "add_one_stake_operator" \
  --args "{ stake_operator_id: 5Grh6bLQmoxinEeiijAfSbGYrYiKhxnxcM2m96s5A64VyAiF, name: \"AZF/GALOIS\", own_staked: 73_132, other_staked: 243_081, commission: 0, total_stakers: 504, vote_points: 0 }" --skip-confirm
 
-cargo contract call --url --url "$NODE_URL" --suri "$AUTHORITY_SEED" --contract $STAKE_VOTING -m "add_one_stake_operator" \
+cargo contract call --url "$NODE_URL" --suri "$AUTHORITY_SEED" --contract $STAKE_VOTING -m "add_one_stake_operator" \
  --args "{ stake_operator_id: 5DATX2UZZgxAsumbVEsmup2q6LR9Bn81F7KW7PsShgUw8t12, name: \"AZF/RIEMANN\", own_staked: 119_877, other_staked: 202_892, commission: 0, total_stakers: 517, vote_points: 0 }" --skip-confirm
 
-cargo contract call --url --url "$NODE_URL" --suri "$AUTHORITY_SEED" --contract $STAKE_VOTING -m "add_one_stake_operator" \
+cargo contract call --url "$NODE_URL" --suri "$AUTHORITY_SEED" --contract $STAKE_VOTING -m "add_one_stake_operator" \
  --args "{ stake_operator_id: 5FnyjESMB4EBQn1W1vnNKZ5oVUYUmQbTPG4hZbJJm8697TKt, name: \"AZF/CAUCHY\", own_staked: 50_355, other_staked: 282_103, commission: 0, total_stakers: 500, vote_points: 0 }" --skip-confirm
 
-cargo contract call --url --url "$NODE_URL" --suri "$AUTHORITY_SEED" --contract $STAKE_VOTING -m "add_one_stake_operator" \
+cargo contract call --url "$NODE_URL" --suri "$AUTHORITY_SEED" --contract $STAKE_VOTING -m "add_one_stake_operator" \
  --args "{ stake_operator_id: 5GN3rbR41UYWtjoxeuyvBfWEPopH4C2R4z7qhtz2ysF5hmrt, name: \"AZF/LAPLACE\", own_staked: 224_547, other_staked: 859_746, commission: 0, total_stakers: 502, vote_points: 0 }" --skip-confirm
